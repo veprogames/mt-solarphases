cb8ce1d0102dc1b5df8575677ad4411868cca012
veprogames <75524847+veprogames@users.noreply.github.com>
2023-04-25 19:49:34 +0200

    balancing, update on every tick, hide sunrise

--- init.lua
+++ init.lua
@@ -1,25 +1,26 @@
 local function update_solar(player)
+    local x = player:get_pos().x
     local z = player:get_pos().z
     local day = minetest.get_day_count() + minetest.get_timeofday()
     local season = day / 30
-    local mag = 3 - math.abs(z) / 31000 * 2
+    -- at z = +-31k, it's north/south pole, where sun height doesn't change over the day
+    local mag = 3 - math.abs(z) / 31000 * 3
     local base = 0.75 + 1.5 * z / 31000 * math.sin(season * 2 * math.pi)
+    -- true noon => 1h earlier to 1h later
+    local time_offset = (1 / 24) * x / 31000
 
     local ratio = base
-        + mag * math.sin((minetest.get_timeofday() - 0.25) * math.pi * 2)
-    print(day, " ", ratio)
+        + mag * math.sin((minetest.get_timeofday() - 0.25 + time_offset) * math.pi * 2)
 
     player:override_day_night_ratio(
         math.max(0, math.min(1, ratio))
     )
-    minetest.after(0.5, function ()
-        update_solar(player)
-    end)
 end
 
 minetest.register_on_joinplayer(function(player, last_login)
     player:set_sun({
-        visible = false
+        visible = false,
+        sunrise_visible = false
     })
     player:set_moon({
         visible = false
@@ -28,7 +29,7 @@ minetest.register_on_joinplayer(function(player, last_login)
         visible = false
     })
     update_solar(player)
-    minetest.after(1, function ()
+    minetest.register_globalstep(function(dtime)
         update_solar(player)
     end)
 end)
